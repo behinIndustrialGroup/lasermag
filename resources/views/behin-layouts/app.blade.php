27c403ed542b2997dd0f228e4016f9bd170019ac
behinIndustrialGroup <info@behin.energy>
2025-11-12 13:19:22 +0330

    update

--- resources/views/behin-layouts/app.blade.php
+++ resources/views/behin-layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" href="{{ url('behin/logo.ico') . '?' . config('app.version') }}">
+    <link rel="icon" href="{{ url('behin/logo.webp') . '?' . config('app.version') }}">
     <link rel="manifest" href="{{ url('manifest.json') . '?' . config('app.version') }}">
 
     <title>@yield('title')</title>
