2fb9063e48422c18394fbbadd39be4d1c9badb07
behinIndustrialGroup <info@behin.energy>
2025-12-31 15:47:37 +0330

    Update add-debit.blade.php

--- packages/behin-simple-workflow-report/src/Views/Core/FinancialTransaction/add-debit.blade.php
+++ packages/behin-simple-workflow-report/src/Views/Core/FinancialTransaction/add-debit.blade.php
@@ -54,26 +54,26 @@
 
 
         <div class="col-sm-4">
-            <div class="form-group"><label>{{ trans('fields.invoice_or_cheque_number') }}</label><input type="text"
-                    name="invoice_or_cheque_number" list="invoice_or_cheque_number_list" value=""
-                    class="form-control" id="invoice_or_cheque_number" placeholder="" style=""></div>
+            <div class="form-group"><label>{{ trans('fields.reference_id') }}</label><input type="text"
+                    name="reference_id" list="reference_id_list" value=""
+                    class="form-control" id="reference_id" placeholder="" style=""></div>
 
         </div>
 
         <div class="col-sm-4">
             <div class="form-group">
-                <label>{{ trans('fields.transaction_or_cheque_due_date') }}</label>
-                <input type="text" name="transaction_or_cheque_due_date" value=""
-                    class="form-control pwt-datepicker-input-element" id="transaction_or_cheque_due_date" placeholder=""
+                <label>{{ trans('fields.date') }}</label>
+                <input type="text" name="date" value=""
+                    class="form-control pwt-datepicker-input-element" id="date" placeholder=""
                     style="" script="">
-                <input type="hidden" name="transaction_or_cheque_due_date_alt" id="transaction_or_cheque_due_date_alt">
+                <input type="hidden" name="date_alt" id="date_alt">
                 <script>
-                    $('#transaction_or_cheque_due_date').persianDatepicker({
+                    $('#date').persianDatepicker({
                         viewMode: 'day',
                         initialValue: false,
                         format: 'YYYY-MM-DD',
                         initialValueType: 'persian',
-                        altField: '#transaction_or_cheque_due_date_alt',
+                        altField: '#date_alt',
                         calendar: {
                             persian: {
                                 leapYearMode: 'astronomical',
